397b39733f011826f35fc144cec7cb6984695d1b
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-11-29 16:35:35 -0700

    Added ios sliders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def slider(name, opts={})
+    id = opts.delete(:id) || name
+    content_tag :label, class: "switch-toggle", for: id do
+      hidden = content_tag :input, "", { type: :hidden, name: name, value: false }
+      input = content_tag :input, "", { type: :checkbox, id: id, name: name, value: true }.merge(opts)
+      switch = content_tag :div, "", class: :switch
+      "#{hidden}#{input}#{switch}".html_safe
+    end
+  end
 end
