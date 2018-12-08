14187c80e30fcbc08ab0042677a2cc14bc1839f2
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-12-07 22:04:46 -0700

    More assets settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,9 @@ Rails.application.configure do
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.digest = true
+  config.serve_static_assets = false
 
   config.active_storage.service = :local
   config.log_level = :debug
