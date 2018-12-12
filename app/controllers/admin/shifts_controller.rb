00895b623e66b83b7be88617ca8c968660b322bf
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-12-12 01:10:45 -0700

    Added history page

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -1,24 +1,50 @@
 class ::Admin::ShiftsController < ::Admin::BaseController
+  before_action :setup
+  before_action { @title = "History" }
 
   def index
-    Shift.order(started_at: :desc)
+    @shifts = Shift.order(started_at: :desc)
+    @shifts = @shifts.where(user_id: params[:user_id]) if params[:user_id].present?
   end
 
-  def show
-    @resource = @resource_type.find(params[:id])
-  end
+  # def new
+  #   @shift = Shift.new
+  #
+  #   render :form
+  # end
+  #
+  # def edit
+  #   render :form
+  # end
+  #
+  # def create
+  #   @shift = Shift.new(shift_params)
+  #
+  #   if @shift.save
+  #     redirect_to [:admin, :shifts]
+  #   else
+  #     render :form
+  #   end
+  # end
+  #
+  # def update
+  #   if @shift.update(shift_params)
+  #     redirect_to [:admin, :shifts]
+  #   else
+  #     render :form
+  #   end
+  # end
+  #
+  private
 
-  def edit
-    @resource = @resource_type.find(params[:id])
+  def setup
+    @shift = Shift.find(params[:id]) if params[:id].present?
   end
-
-  def update
-    @resource = @resource_type.find(params[:id])
-
-    if @resource.update(resource_params)
-      redirect_to :admin, @resource
-    else
-      redirect_to :admin, :edit, @resource
-    end
-  end
+  #
+  # def shift_params
+  #   params.require(:shift).permit(
+  #     :name,
+  #     :available
+  #   )
+  # end
 end
